681ec55e0e79d1836db1c89149ff6dd479da99de
StlMaris123 <stellamarisnjage@gmail.com>
2020-05-15 16:55:48 +0200

    implement texts and their replies

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,26 @@
+class CommentsController < ApplicationController
+        before_action :find_comment
+    
+      def new
+        @message = Message.find(params[:id])
+        @comment = Comment.new
+      end
+    
+      def create
+        @comment.comments.build(comment_params)
+        if @comment.save
+        else
+        end
+      end
+    
+      private
+    
+      def comment_params
+        params.require(:comment).permit(:message)
+      end
+    
+      def find_comment
+        @comment = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
+        @comment = Message.find_by_id(params[:message_id]) if params[:message_id]
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :messages
+  root 'messages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :messages do
+    resources :comments
+  end
+
+  resources :comments do
+    resources :comments
+  end
 end
